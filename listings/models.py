b98d7fbfcea4f2e6024da09e6a0345084d5993a8
Rafał Zalewski <zalfik@gmail.com>
2022-04-01 19:14:53 +0200

    Pull data, display template, pagination

--- listings/models.py
+++ listings/models.py
@@ -17,7 +17,7 @@ class Listing(models.Model):
     garage = models.IntegerField(default=0)
     sqft = models.IntegerField()
     lot_size = models.DecimalField(max_digits=5, decimal_places=1)
-    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/')
     photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
